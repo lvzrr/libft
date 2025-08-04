8f5785eef7f491bb577404d0cc0366cebda787ba
lvzrr <lvzrr@proton.me>
2025-08-04 14:37:04 +0200

    hash function

--- src/mem/ft_hash.c
+++ src/mem/ft_hash.c
@@ -14,18 +14,26 @@
 
 t_u128	ft_hash(t_u8 *__restrict__ bytes, size_t n)
 {
-	t_u128	hash;
-	t_u128	x;
-	size_t	i;
+	t_u128			hash;
+	static t_u128	table[256] = {0};
+	size_t			i;
 
 	hash = HASH_SEED;
 	i = 0;
+	if (!table[0])
+	{
+		while (i < 256)
+		{
+			table[i] = ~(i << 8 | i << 16 | i << 32
+					| (t_u128)i << 64) * HASH_PRIME;
+			++i;
+		}
+	}
+	i = 0;
 	while (i++ < n)
 	{
-		x = *bytes << 8 | *bytes << 16
-			| (t_u64) * bytes << 32 | (t_u128) * bytes << 64;
-		hash ^= ~x * HASH_PRIME;
-		hash *= hash;
+		hash ^= table[*bytes];
+		hash ^= hash << 64;
 		hash = (hash << 5) | (hash >> (128 - 5));
 		++bytes;
 	}
