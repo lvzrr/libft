50688c60d25345a0f08ad93548fdbb55cc9fda21
lvzrr <lvzrr@proton.me>
2025-08-04 13:19:16 +0200

    qfix

--- src/mem/ft_membroadcast.c
+++ src/mem/ft_membroadcast.c
@@ -26,7 +26,7 @@ void	ft_membroadcast(void *dst, void *src, size_t chunks, size_t n)
 		ft_memcpy((t_u8 *)dst + i, src, chunks);
 		i += chunks;
 	}
-	while (i < n)
+	while (i + chunks <= n)
 	{
 		ft_memcpy((t_u8 *)dst + i, src, chunks);
 		i += chunks;
